9cd9290e85ec01f9555b9ab3cbf5fcb3fab88274
unknown <bmaitner@gmail.com>
2015-09-23 15:14:09 -0700

    added helpful text about viewing old versions

--- example.R
+++ example.R
@@ -1,3 +1,8 @@
 #change 1
 
 #change 2
+
+#to view old versions, changes:
+#click on Git tab, choose the "History" option
+#in the box that opens, click on "History"
+# to view an old version,click on one of the "subjects", then click "view file @ <whatever>"
