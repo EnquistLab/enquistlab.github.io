4604bc07add820fdd0ec67e984f68521fe57b475
Daniel Guaderrama <dguaderrama@email.arizona.edu>
2015-10-08 07:02:39 -0700

    Added GIS 101,1 and Mac GIS Tools

---
+++ _posts/2015-10-08-mac-gis-tools.md
@@ -0,0 +1,116 @@
+---
+layout: post
+title: "Mac GIS tools"
+description: ""
+category: 
+tags: []
+---
+{% include JB/setup %}
+
+# Mac GIS Tools
+
+This is documentation on how to configure your Mac system for GIS tools.
+
+## Install brew.sh - the missing package manager for OS X
+
+<a href="http://brew.sh" target="_blank">http://brew.sh</a>
+
+Homebrew installs the stuff you need that Apple didn't.
+
+Follow the instructions. Requires termial: 
+
+``COMMAND + spacebar > search 'terminal'``
+
+Paste command:
+
+``ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"``
+
+Confirm install:
+
+``$ brew --help``
+
+## Easy installations:
+
+QGIS requires: Python2.7, GDAL, PROJ.4, NumPy, SciPy, Matplotlib
+
+Install the libraries/software in the order below.
+
+- To allow unrecognized developers/apps to be installed: CONTROL + CLICK on .pkg to open
+
+GDAL:
+
+- <a href="http://www.kyngchaos.com/files/software/frameworks/GDAL_Complete-1.11.dmg" target="_blank">GDAL 1.11 Complete</a>
+
+PROJ:
+
+- <a href="http://www.kyngchaos.com/files/software/frameworks/PROJ_Framework-4.9.2-1.dmg" target="_blank">PROJ.4</a>
+
+NumPy:
+
+Included on the GDAL Framework disk image.
+
+- <a href="http://www.kyngchaos.com/files/software/python/NumPy-1.9.2-1.dmg" target="_blank">NumPy 1.9.2-1</a>
+- <a href="http://www.kyngchaos.com/files/software/python/NumPy-1.8.0-1.dmg" target="_blank">NumPy 1.8.0-1 (32+64bit, Snow Leopard+)</a>
+
+**Matplotlib:**
+
+Requires: NumPy
+
+- <a href="http://www.kyngchaos.com/files/software/python/matplotlib-1.4.3-1.dmg">matplotlib 1.4.3-1</a>
+
+**QGIS:**
+
+- <a href="http://www.kyngchaos.com/files/software/qgis/QGIS-2.10.1-1.dmg">QGIS 2.10.1-1</a>
+
+
+#### Optional Python modules:
+
+**SciPy:**
+
+Requires: NumPy
+
+- <a href="http://www.kyngchaos.com/files/software/python/SciPy-0.16.0-1.dmg" target="_blank">SciPy 0.16.0-1</a>
+- <a href="http://www.kyngchaos.com/files/software/python/SciPy-0.13.1-1.dmg" target="_blank">SciPy 0.13.1-1 (32+64bit, Snow Leopard+)</a>
+
+**Python Imaging Library (PIL):**
+
+Requires: UnixImageIO, FreeType
+
+- <a href="http://www.kyngchaos.com/files/software/python/PIL-1.1.7-4.dmg" target="_blank">PIL 1.1.7-4 (32+64bit, Snow Leopard+)</a>
+
+**psycopg2:**
+
+PostgreSQL interface
+
+- <a href="http://www.kyngchaos.com/files/software/python/Psycopg2-2.4.5-2.dmg">Psycopg2 2.4.5-2</a>
+
+**RPy2:**
+
+Requires: R 3.1
+
+- <a href="http://www.kyngchaos.com/files/software/python/RPy2-2.5.4-1.dmg">RPy2 2.5.4-1 for R 3.1</a>
+
+**Pythoni Spatial Analysis Library (PySAL):**
+
+Requires: NumPy, SciPy
+
+``$ sudo easy_install pysal``
+
+## GDAL for terminal
+
+This installation will allow command line access to GDAL for scripting geoprocessing and PostgreSQL support. BuildingOnMac documentation is located <a href="https://trac.osgeo.org/gdal/wiki/BuildingOnMac" target="_blank">here</a>.
+
+Open terminal:
+
+``COMMAND + SPACEBAR > search 'terminal'``
+
+Install GDAL:
+
+This one will take some time.
+
+``$ brew install gdal --with-unsupported --with-libkml --with-postgresql-9.4``
+
+Confirm install: 
+
+``$ gdalinfo``
+
---
+++ _site/GIS/R_gis.R
@@ -0,0 +1,105 @@
+#first, install and run all this stuff, because I said so.  
+#Also, it might be useful:
+
+require(rgdal)
+require(maptools)
+require(mapproj)
+require(raster)
+require(sp)
+require(devtools)
+require(taxize)
+require(ape)
+require(Hmisc)
+require(maps)
+#although I hate to do it this way, for the sake of this class, let's set our working directory:
+setwd("C:/Users/Brian/Desktop/BIEN_API_WORK/test_junk/enquistlab.github.io/GIS/")
+
+
+#let's start by plotting a basic map using the maps functions.
+#just type:
+
+map() #simple as that, since the default is a world map!
+map('state')# is another option
+#or, if we want it a bit prettier:
+map('world',bg = "light blue",col="green",fill=TRUE)#specifying colors is pretty basic, but often helps with visualization
+
+#So, we know what the world looks like, more or less.  Let's see some organisms!
+
+#Let's start by loading a shapefile range map for a randomly chosen species:
+
+cannabis_sativa<-readShapePoly("plant_examples/Cannabis_sativa.shp")#obviously you'll need to change this a bit for your computer
+str(cannabis_sativa)#tells us about the structure of the data associated with this map.  Not much here for this simple shapefile,though
+cannabis_sativa@proj4string#tells us whether the coordinate reference system is set and whether there is a projection
+cannabis_sativa@data#has the species listed.  not very impressive, but useful if you have a lot of maps!
+#so, where does this species live?
+plot(cannabis_sativa)#not very useful, eh?  try:
+map('world',bg = "light blue",col="grey",fill=TRUE)
+plot(cannabis_sativa,col="green",add=TRUE)
+
+#hmmm, maybe we want to zoom in on the area where the species is found?
+
+plot(cannabis_sativa,col="green")
+map('world',add=TRUE)
+
+#let's try another species:
+acer_rubrum<-readShapePoly("plant_examples/Acer_rubrum.shp")
+#let's add this one
+plot(acer_rubrum,col="dark green", add=TRUE)
+
+#lets plot JUST the areas of overlap
+map('world',bg = "light blue",col="grey",fill=TRUE)
+plot(gIntersection(acer_rubrum,cannabis_sativa),add=TRUE,col="green")#this maps JUST the areas that are in common between the two
+
+
+#what about occurrence points?
+load("plant_examples/cannabis_sativa_occurrences")
+
+#easy, just add the points as you would with any normal plot
+map('world',bg = "light blue",col="grey",fill=TRUE)
+plot(cannabis_sativa,col="light green",add=TRUE)
+points(cannabis_sativa_occurrences$longitude,cannabis_sativa_occurrences$latitude,pch=16,cex=.25,col="red")
+
+
+#raster data: climate
+bioclim1<-raster("bio_10m_bil/bio1.bil")
+#okay, guess how we plot it?
+plot(bioclim1)
+plot(cannabis_sativa,add=TRUE)
+# what if we want to know something about where this plant grows?
+cannabis_sativa_bioclim1_values<-extract(bioclim1,cannabis_sativa)#this will retrieve all values of the raster that intersect with the shapefile range map
+hist(unlist(cannabis_sativa_bioclim1_values))#we could likewise get the mean, variance, whatever we want
+
+cannabis_sativa_points<-SpatialPoints(na.omit(cbind(cannabis_sativa_occurrences$longitude,cannabis_sativa_occurrences$latitude)))#put the occurrence points into a spatial points file, needed for the extract function
+cannabis_sativa_bioclim1_values_points<-extract(bioclim1,cannabis_sativa_points)#this function retreives the values at all points for the spatial points file
+hist(cannabis_sativa_bioclim1_values_points)
+
+
+#IUCN data:combined shapefiles!
+caecillians<-readShapePoly("GYMNOPHIONA/GYMNOPHIONA.shp")
+
+#These shapefile have more species associated with them and more metadata, lets take a look!
+str(caecillians)
+caecillians@data$binomial
+
+#Note that conveniently these shapefiles are in a dataframe, so typical dataframe indexing works with them
+
+plot(caecillians)#plots all shapefiles
+map('world',xlim = c(-90,-45),ylim=c(0,15),fill=TRUE,col="light green")
+plot(caecillians[1,],col="brown",add=TRUE)#plots only the first shapefile
+caecillians[1,]@data$binomial#get the species name for the first shapefile
+
+blank_raster<-setValues(bioclim1,0)
+diversity_output<-setValues(blank_raster,0)#create a blank raster to store output data
+for(i in 1:length(caecillians)){
+  print(i)
+  range_i<-caecillians[i,] #extracts the ith range map from the dataframe
+  sp_i<-as.character(range_i@data$BINOMIAL) #gets the species associated with the ith range map    
+  #rasterized_i<-rasterize(range_i,raster)
+  rasterized_i<-rasterize(range_i,blank_raster)
+  rasterized_i[which(is.na(getValues(rasterized_i))==TRUE)]<-0
+  diversity_output<-diversity_output+rasterized_i
+  plot(diversity_output) 
+}
+
+
+plot(getValues(diversity_output)~getValues(bioclim1))
